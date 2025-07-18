bd6cfc92a4cb93fb8650bb211edf264ff9853a14
Bhargavvv412 <bhargavvv412@gmail.com>
2025-07-18 22:12:00 +0530

    derv

---
+++ Basic-Data-Structure/Array/Easy/Chocolate-Distribution-Problem.cpp
@@ -0,0 +1,15 @@
+    // Input: arr[] = {7, 3, 2, 4, 9, 12, 56}, m = 3 
+    // Output: 2 
+    // Explanation: If we distribute chocolate packets {3, 2, 4}, we will get the minimum difference, that is 2. 
+
+    // Input: arr[] = {7, 3, 2, 4, 9, 12, 56}, m = 5 
+    // Output: 7
+    // Explanation: If we distribute chocolate packets {3, 2, 4, 9, 7}, we will get the minimum difference, that is 9 - 2 = 7. 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    return 0;
+}
