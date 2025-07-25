a55396bf083a1ee8a123d00fd51cad045d121e05
Bhargavvv412 <bhargavvv412@gmail.com>
2025-07-26 00:40:07 +0530

    dsa

--- Basic-Data-Structure/Array/Meadium/Buy-Sell-Stock.cpp
+++ Basic-Data-Structure/Array/Meadium/Buy-Sell-Stock.cpp
@@ -4,9 +4,29 @@
 // Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
 
 
+
+
+
 #include <iostream>
+#include <vector>
 using namespace std;
 
+
+
+int profit(vector<int>& arr){
+    int profit = 0;
+    int buyPrice = arr[0];
+    for(int i = 0;i<arr.size();i++){
+        if (buyPrice>arr[i]){
+            buyPrice = arr[i];
+        }
+        profit = max(profit,arr[i]-buyPrice);
+    }
+    return profit;
+}
+
+
 int main(){
-    vector<int>
+    vector<int> arr ={7,1,5,3,6,4};
+    cout << profit(arr);
 }
